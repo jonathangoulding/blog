16d2fd5e4d8c452fdd0db3c47cec6220ee85ba80
jonathangoulding <jonathangoulding21@gmail.com>
2021-03-26 20:37:33 +0000

    fix: genearte html files not SPA

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,9 @@
 export default {
-  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
-
-  // Target: https://go.nuxtjs.dev/config-target
+  ssr: true,
   target: 'static',
-
-  // Global page headers: https://go.nuxtjs.dev/config-head
+  router: {
+    base: '/blog/'
+  },
   head: {
     title: 'blog',
     htmlAttrs: {
@@ -21,34 +19,17 @@ export default {
       { rel: 'stylesheet', type: 'text/css', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css' }
     ]
   },
-
-  router: {
-    base: '/blog/'
-  },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     'bulma'
   ],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
   ],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module'
   ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
   ],
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   }
 }
