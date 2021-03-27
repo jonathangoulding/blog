2616a1793c4c6e2774e65d9b564061703cb96cfb
jonathangoulding <jonathangoulding21@gmail.com>
2021-03-27 09:40:24 +0000

    feat: introduce blog snippet component

---
+++ components/blog/snippet.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="section">
+    <div class="columns">
+      <div class="column is-8 is-offset-2">
+        <div class="content is-medium">
+          <h2 class="subtitle is-4">
+            {{ date }}
+          </h2>
+          <h1 class="title">
+            {{ title }}
+          </h1>
+          <slot />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ['title', 'date', 'para']
+}
+</script>
